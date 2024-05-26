a168b4b23f47c0606cd2400625cd675ebc3c873a
MikeDEV <mazurkevich.mikhail.14@gmail.com>
2024-05-26 19:23:25 +0200

    modify description empty error

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -38,7 +38,9 @@ class RandomChar extends Component {
 					/>
 					<div className='randomchar__info'>
 						<p className='randomchar__name'>{name}</p>
-						<p className='randomchar__descr'>{description}</p>
+						<p className='randomchar__descr'>
+							{description === '' ? '📌 no description' : description}
+						</p>
 						<div className='randomchar__btns'>
 							<a href={homepage} className='button button__main'>
 								<div className='inner'>homepage</div>
