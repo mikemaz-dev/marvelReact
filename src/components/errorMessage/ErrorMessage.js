126193d34033122e759f98d7e9c8afa88fcfff0c
MikeDEV <mazurkevich.mikhail.14@gmail.com>
2024-05-26 21:35:04 +0200

    Add spinner when loading data and error message when error

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,18 @@
+import img from './error.gif'
+
+const ErrorMessage = () => {
+	return (
+		<img
+			style={{
+				display: 'block',
+				width: '250px',
+				objectFit: 'contain',
+				margin: '0 auto',
+			}}
+			src={img}
+			alt='error message'
+		/>
+	)
+}
+
+export default ErrorMessage
