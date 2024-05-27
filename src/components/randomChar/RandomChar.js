1c9960d5d05f95a3816727f53a8986e81bf9d94a
MikeDEV <mazurkevich.mikhail.14@gmail.com>
2024-05-27 19:08:55 +0200

    added class to thumbnail if image for character is not found

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -68,9 +68,16 @@ class RandomChar extends Component {
 
 const View = ({ char }) => {
 	const { name, description, thumbnail, homepage, wiki } = char
+	let thumbnailClass = 'randomchar__img'
+	if (
+		thumbnail ===
+		'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg'
+	) {
+		thumbnailClass += '-notfound'
+	}
 	return (
 		<div className='randomchar__block'>
-			<img src={thumbnail} alt='Random character' className='randomchar__img' />
+			<img src={thumbnail} alt='Random character' className={thumbnailClass} />
 			<div className='randomchar__info'>
 				<p className='randomchar__name'>{name}</p>
 				<p className='randomchar__descr'>
