40c852e775dfd036926c95da5e366f83c2c40e06
MikeDEV <mazurkevich.mikhail.14@gmail.com>
2024-05-28 18:09:13 +0200

    limit the number of chars

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,7 +12,8 @@ class CharList extends Component {
 		loading: true,
 		error: false,
 		newItemLoading: false,
-		offset: 210,
+		offset: 1541,
+		charEnded: false,
 	}
 	marvelService = new MarvelService()
 
@@ -34,11 +35,18 @@ class CharList extends Component {
 	}
 
 	onCharListLoaded = newCharList => {
+		let ended = false
+
+		if (newCharList.length < 9) {
+			ended = true
+		}
+
 		this.setState(({ offset, charList }) => ({
 			charList: [...charList, ...newCharList],
 			loading: false,
 			newItemLoading: false,
 			offset: offset + 9,
+			charEnded: ended,
 		}))
 	}
 
@@ -75,11 +83,12 @@ class CharList extends Component {
 	}
 
 	render() {
-		const { charList, loading, error, offset, newItemLoading } = this.state
+		const { charList, loading, error, offset, newItemLoading, charEnded } = this.state
 		const items = this.renderItems(charList)
 		const errorMessage = error ? <ErrorMessage /> : null
 		const spinner = loading ? <Spinner /> : null
 		const content = !(loading || error) ? items : null
+
 		return (
 			<div className='char__list'>
 				{spinner}
@@ -88,6 +97,7 @@ class CharList extends Component {
 				<button
 					className='button button__main button__long'
 					disabled={newItemLoading}
+					style={{'display' : charEnded ? 'none' : 'block'}}
 					onClick={() => this.onRequest(offset)}
 				>
 					<div className='inner'>load more</div>
